ea88a4c3f2f8baef96dd60de3b8a910e0aba750f
Okwuchi Egeolu <okwuchiegeolu@gmail.com>
2023-03-02 12:25:11 +0100

    Second Commit

---
+++ index.js
@@ -0,0 +1,61 @@
+//Take Home Assignment for week 2
+// 1.
+// for(let i = 25; i >= 0; i -= 5) {
+//     console.log(i)
+// }
+
+//2.
+
+// const people = ["Scooby", "Velma", "Daphne", "Shaggy", "Fred"];
+// for(let i = 0; i < people.length; i++) {
+//     console.log(i,people[i]);
+
+
+//3.
+
+// function printInstructions(){
+//     const Instructions= ([
+//     "Step 1 - Remove the plastic wrapper",
+//     "Step 2- Microwave 45 seconds",
+//     "Step 3 - Let cool",
+//     "Step 4 - Enjoy!",
+//     ]);
+//     for(let i = 0; i < Instructions.length; i++){
+//         console.log(Instructions[i]);
+//     }
+// }
+
+// printInstructions();
+
+// let sum = 0;
+// for(i = 1; i <= 10; i++) {
+//     sum = sum + i;
+// }
+// console.log(sum);
+
+
+
+//4.
+
+// let secretword =  prompt('Jesus Loves Me');
+// while(true) {
+//     secretword = prompt(secretword);
+//     if(secretword.toLowerCase === 'yes i know')break;
+// }
+// console.log('You won!');
+
+// const secretword = 'Yes I know';
+// let guess = prompt('Enter the secret word');
+// while(secretword !== guess){
+//     guess= prompt('Enter the secret word'); 
+// }
+
+// console.log('Alright you won');
+
+
+
+
+
+
+
+
